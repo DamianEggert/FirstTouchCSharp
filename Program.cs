ca7c8a71250f1612eaf882223f54b1101e437bd7
DamianEggert <55459525+DamianEggert@users.noreply.github.com>
2019-09-19 20:57:27 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "developer";
+            string guess = "";
+            int chanceCount = 0;
+            int chanceLimit = 5;
+            bool outOfChance = false;
+
+            do
+            {
+                if(chanceCount < chanceLimit)
+                {
+                    Console.Write("Enter guess word: ");
+                    guess = Console.ReadLine();
+                    chanceCount++;
+                } else
+                {
+                    outOfChance = true;
+                }
+            } while (guess != secretWord && !outOfChance);
+
+            if (!outOfChance)
+            {
+                Console.WriteLine("You win!");
+            } else
+            {
+                Console.WriteLine("You lose!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
