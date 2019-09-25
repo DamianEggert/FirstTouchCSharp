af642db15b7e163366629b50ee024a820176886a
DamianEggert <55459525+DamianEggert@users.noreply.github.com>
2019-09-25 21:05:20 +0200

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex._3
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+        private string rating;
+
+        public Book()
+        {
+
+        }
+
+        public Book(string aTitle, string aAuthor, int aPages, string aRating)
+        {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+            Rating = aRating;
+        }
+
+        public string Rating
+        {
+            get { return rating; }
+            set
+            {
+                if (value == "good" || value == "bad")
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "anyway good";
+                }
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("GoT", "George", 1200, "good");
+            Book book2 = new Book("LOTR", "Tolkien", 1200, "bad");
+            Book book3 = new Book("Lalka", "Mickiewicz", 1200, "asd");
+
+            Console.WriteLine(book1.Rating);
+            Console.WriteLine(book2.Rating);
+            Console.WriteLine(book3.Rating);
+            Console.ReadLine();
+        }
+    }
+}
